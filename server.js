c04337e5c8675dfa56b69fbf4c06c31d88bc9f44
Lekha Surasani <lesurasani@gmail.com>
2019-01-13 14:40:14 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Your server is up and running on port ${port}`))
+
+app.get('/express_backend', (req, res) => {
+    request.get({url: "http://barracks.martaarmy.org/ajax/get-next-departures.php?stopid=901230"}, function(err, response, body) {
+        if (!err) {
+            res.send({ body: JSON.parse(body) })
+        }
+    })
+})
+
+
+
+
